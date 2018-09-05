85e900c070f6e4b2a832a9fdd8443636944195ea
phyxolog <yzhivaga@gmail.com>
2018-09-05 16:13:52 +0300

    Impl Logger

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,12 +1,12 @@
 const LoggerType = ["info", "warn", "error"];
 
 interface ILogger {
-  [key: string]: any
+  [key: string]: any;
 }
 
 const Logger: ILogger = {};
 
-LoggerType.forEach(type => {
+LoggerType.forEach((type) => {
   Logger[type] = (message: string, ...params: Array<any>) => {
     console[type].call(null, message, ...params);
   };
